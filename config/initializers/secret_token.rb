7f223a5356f992f12ab0efb5dfd1956aeedca75d
Raghu Betina <rbetina@gmail.com>
2013-06-24 01:04:52 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Listsign::Application.config.secret_key_base = 'a941369b9a0e8147c0ef42a58e8bb2fb08a8579a27f76d25a42669d0d0f325c3a1f17c528f5e1f426d28da09545bb162c6f3f7cc6d4341e2886980462382d8f2'
